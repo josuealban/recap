200cd6058aa12e80bec52e2b24dfd872b23c5e58
josuealban <mjalban@sudamericano.edu.ec>
2024-11-28 10:51:44 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Pedir la edad al usuario
+let edad = prompt("¿Cuántos años tienes?");
+
+// Verificar si la edad es 18 o más
+if (edad >= 18) {
+  alert("Puedes votar.");
+} else {
+  alert("No puedes votar.");
+}
